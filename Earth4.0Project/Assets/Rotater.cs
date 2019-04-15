7435c0d87cb40fb5ab7912208427bbb03e63c345
alexander kennedy <rexer1998@gmail.com>
2019-04-15 16:09:14 -0700

    Earth now rotates

---
+++ Earth4.0Project/Assets/Rotater.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 leftStick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        Debug.Log(leftStick);
+
+    }
+}
---
+++ Earth4.0Project/Assets/Scripts/WorldRotater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldRotater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 leftStick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        Debug.Log(leftStick);
+        float leftRotate = Time.deltaTime * leftStick.x * 90;
+        float rightRotate = Time.deltaTime * leftStick.y * 90;
+        transform.Rotate(0, leftRotate, rightRotate, Space.World);
+    }
+}
