ab951dfd25cbbfc439a9b66cc42c516cbd8402bb
Alexander Kennedy <scu.imaginarium@gmail.com>
2019-04-17 17:11:45 -0700

    Create GameManager script, increase pollution over time based on number of factories

---
+++ Earth4.0Project/Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    // Game Values;
+    public float currentPollution = 0.0f;
+
+    // Constants
+    const float factoryPollutionPerSecond = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        int numFactories = GameObject.FindGameObjectsWithTag("Factory").Length;
+        currentPollution += numFactories * factoryPollutionPerSecond * Time.deltaTime;
+
+        Debug.Log(currentPollution);
+
+	}
+}
