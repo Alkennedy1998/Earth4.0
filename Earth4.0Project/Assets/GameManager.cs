d6ae6897cb4ebe1de6b24b97278369ab3ec816a4
Alexander Kennedy <scu.imaginarium@gmail.com>
2019-04-17 17:42:35 -0700

    GameManager: Add other rescources
    Money, Food, Cotton

--- Earth4.0Project/Assets/GameManager.cs
+++ Earth4.0Project/Assets/GameManager.cs
@@ -6,6 +6,9 @@ public class GameManager : MonoBehaviour {
 
     // Game Values;
     public float currentPollution = 0.0f;
+    public float currentMoney = 0.0f;
+    public float currentFood = 0.0f;
+    public float currentCotton = 0.0f;
 
     // Constants
     const float factoryPollutionPerSecond = 1.0f;
