7c5d42f710929639dc70fc68bf7dad380e0193a8
MattMistele <matthew@mistele.com>
2019-04-23 10:24:31 -0700

    Remove watch hand component

--- Earth4.0Project/Assets/GameManager.cs
+++ Earth4.0Project/Assets/GameManager.cs
@@ -6,50 +6,50 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     // Constants
-    private const float tickTime = 5.0f; // There are 5 seconds between 'ticks'
-    public const float factoryPollutionPerTick = 1.0f;
-    public const float farmFoodPerTick = 1.0f;
+    private const float _tickTime = 5.0f; // There are 5 seconds between 'ticks'
+    public const float _factoryPollutionPerTick = 1.0f;
+    public const float _farmFoodPerTick = 1.0f;
 
     // Internal Values
-    private float currentTickTime = 0.0f;
+    private float _currentTickTime = 0.0f;
 
     // Game Values
-    public float currentPollution = 0.0f;
-    public float currentMoney = 0.0f;
-    public float currentFood = 0.0f;
-    public float currentCotton = 0.0f;
+    public float _currentPollution = 0.0f;
+    public float _currentMoney = 0.0f;
+    public float _currentFood = 0.0f;
+    public float _currentCotton = 0.0f;
 
     // Game Objects
-    public List<GameObject> factoryList = new List<GameObject>();
-    public List<GameObject> farmList = new List<GameObject>();
-    public List<GameObject> houseList = new List<GameObject>();
-    public List<GameObject> treeList = new List<GameObject>();
+    public List<GameObject> _factoryList = new List<GameObject>();
+    public List<GameObject> _farmList = new List<GameObject>();
+    public List<GameObject> _houseList = new List<GameObject>();
+    public List<GameObject> _treeList = new List<GameObject>();
 
-    private GameObject moneyText, foodText, cottonText;
+    private GameObject _moneyText, _foodText, _cottonText;
 
 	// Use this for initialization
 	void Start () {
-        moneyText = GameObject.Find("MoneyText");
-        foodText = GameObject.Find("FoodText");
-        cottonText = GameObject.Find("CottonText");
+        _moneyText = GameObject.Find("MoneyText");
+        _foodText = GameObject.Find("FoodText");
+        _cottonText = GameObject.Find("CottonText");
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        currentTickTime += Time.deltaTime;
+        _currentTickTime += Time.deltaTime;
 
-        if (currentTickTime >= tickTime)
+        if (_currentTickTime >= _tickTime)
             onTick();
         
 	}
 
     private void onTick()
     {
-        currentTickTime = 0f;
+        _currentTickTime = 0f;
 
-        currentPollution += factoryList.Count * factoryPollutionPerTick;
-        currentFood += farmList.Count * farmFoodPerTick;
+        _currentPollution += _factoryList.Count * _factoryPollutionPerTick;
+        _currentFood += _farmList.Count * _farmFoodPerTick;
 
         updateText();
 
@@ -58,18 +58,18 @@ public class GameManager : MonoBehaviour {
 
     private void updateText()
     {
-        moneyText.GetComponent<TextMeshPro>().text = currentMoney.ToString();
-        foodText.GetComponent<TextMeshPro>().text = currentFood.ToString();
-        cottonText.GetComponent<TextMeshPro>().text = currentCotton.ToString();
+        _moneyText.GetComponent<TextMeshPro>().text = _currentMoney.ToString();
+        _foodText.GetComponent<TextMeshPro>().text = _currentFood.ToString();
+        _cottonText.GetComponent<TextMeshPro>().text = _currentCotton.ToString();
     }
 
-    public void addFactory(GameObject factory) { factoryList.Add(factory); }
-    public void addFarm(GameObject farm) { farmList.Add(farm); }
-    public void addHouse(GameObject house) { houseList.Add(house); }
-    public void addTree(GameObject tree) { treeList.Add(tree); }
+    public void addFactory(GameObject factory) { _factoryList.Add(factory); }
+    public void addFarm(GameObject farm) { _farmList.Add(farm); }
+    public void addHouse(GameObject house) { _houseList.Add(house); }
+    public void addTree(GameObject tree) { _treeList.Add(tree); }
 
     private void logValues()
     {
-        Debug.Log("Pollution: " + currentPollution + "     Money: " + currentMoney + "     Food: " + currentFood + "     Cotton: " + currentCotton);
+        Debug.Log("Pollution: " + _currentPollution + "     Money: " + _currentMoney + "     Food: " + _currentFood + "     Cotton: " + _currentCotton);
     }
 }
