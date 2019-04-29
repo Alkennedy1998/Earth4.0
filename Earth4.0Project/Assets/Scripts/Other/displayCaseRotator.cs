d9d44f24d7f70b66a3fe8d014e49175fcab12be4
Mohammed Khadadeh <mkhadadeh@scu.edu>
2019-04-29 15:49:54 -0700

    Rotating display case now functioning

---
+++ Earth4.0Project/Assets/Scripts/Other/displayCaseRotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class displayCaseRotator : MonoBehaviour {
+
+	private float rotateSpeed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+	}
+}
