6ac04833ce0ba641c29fcf93dac32d8287da0db9
tshur <tim.shur@gmail.com>
2019-05-02 10:11:22 -0700

    Update WorkableBuildingScript
    Change isFull/isEmpty to public variables

--- Earth4.0Project/Assets/Scripts/BuildingScripts/WorkableBuildingScript.cs
+++ Earth4.0Project/Assets/Scripts/BuildingScripts/WorkableBuildingScript.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class _WorkableBuildingScript : MonoBehaviour {
 
-	private const int MAX_WORKERS = 5;
+	private const int _MAX_WORKERS = 5;
 
-	public int currentWorkers;
+	public bool _isFull;
+	public bool _isEmpty;
+	public int _currentWorkers;
 
 	// Use this for initialization
 	public void Start () {
-		currentWorkers = 0;
+		_currentWorkers = 0;
+		_isFull = false;
+		_isEmpty = true;
 	}
 
 	// Update is called once per frame
@@ -18,25 +22,27 @@ public class _WorkableBuildingScript : MonoBehaviour {
 
 	}
 
-	public bool isFull() {
-		return currentWorkers >= MAX_WORKERS;
-	}
-
-	public bool isEmpty() {
-		return currentWorkers <= 0;
-	}
-
 	public bool addWorker() {
-		if (isFull())
+		if (_isFull)
 			return false;
-		currentWorkers++;
+
+		_currentWorkers++;
+		if (_currentWorkers >= _MAX_WORKERS)
+			_isFull = true;
+		if (_isEmpty)
+			_isEmpty = false;
 		return true;
 	}
 
 	public bool removeWorker() {
-		if (isEmpty())
+		if (_isEmpty)
 			return false;
-		currentWorkers--;
+
+		_currentWorkers--;
+		if (_currentWorkers <= 0)
+			_isEmpty = true;
+		if (_isFull)
+			_isFull = false;
 		return true;
 	}
 }
