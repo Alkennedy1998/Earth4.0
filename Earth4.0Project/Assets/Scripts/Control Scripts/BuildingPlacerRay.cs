0713506fb0c86b835b21a06a6d14a8a2c783f293
Alexander Kennedy <rexer1998@gmail.com>
2019-05-01 22:13:29 -0700

    Building placement is now restricted to area without other objects. Building highlight before placement also implemented

--- Earth4.0Project/Assets/Scripts/Control Scripts/BuildingPlacerRay.cs
+++ Earth4.0Project/Assets/Scripts/Control Scripts/BuildingPlacerRay.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class BuildingPlacerRay : MonoBehaviour
 {
-
+    #region gameValues
     public float _length_multiplier = 10;
     public float _lineWidth = 0.3f;
 
     private LineRenderer _lineRenderer;
+
+    private GameObject _outlineBuilding = null;
+    public int _buildingCollisions = 0;
 
     private GameObject _world;
     private int _layerMask;
@@ -20,6 +23,9 @@ public class BuildingPlacerRay : MonoBehaviour
 
     private bool triggerHasBeenReleased = true;
 
+    #endregion
+
+    #region unityFunctions
     void Start()
     {
 
@@ -42,24 +48,105 @@ public class BuildingPlacerRay : MonoBehaviour
         rayCollisionHandler();
 
     }
+    #endregion
 
     void rayCollisionHandler()
     {
+
+        //If ray hits an object
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out _hit, Mathf.Infinity, _layerMask))
         {
-
             var collidedObject = _hit.collider.gameObject;
+            Quaternion rotation = Quaternion.LookRotation(_hit.normal);
 
             //Make line visible to user and turn green
             _lineRenderer.SetPosition(1, Vector3.forward * _hit.distance);
             _lineRenderer.material.color = Color.green;
 
-            //If collider intersects a button set active Item to that button item
+            #region buildingHighlighter
+            if (collidedObject.name == "World")
+            {
+                //Instantiate a building if none exists yet
+                if (_outlineBuilding == null)
+                {
+
+                    if (_equippedBuilding == Buildings.None)
+                    {
+                        return;
+                    }
+                    else if (_equippedBuilding == Buildings.Factory)
+                    {
+                        _outlineBuilding = _world.GetComponent<GameManager>().instantiateOnWorld(_world.GetComponent<GameManager>()._factoryPrefab, _hit.point, rotation);
+                    }
+                    else if (_equippedBuilding == Buildings.Farm)
+                    {
+                        _outlineBuilding = _world.GetComponent<GameManager>().instantiateOnWorld(_world.GetComponent<GameManager>()._farmPrefab, _hit.point, rotation);
+                    }
+                    else if (_equippedBuilding == Buildings.House)
+                    {
+                        _outlineBuilding = _world.GetComponent<GameManager>().instantiateOnWorld(_world.GetComponent<GameManager>()._housePrefab, _hit.point, rotation);
+                    }
+                    else if (_equippedBuilding == Buildings.Tree)
+                    {
+                        _outlineBuilding = _world.GetComponent<GameManager>().instantiateOnWorld(_world.GetComponent<GameManager>()._treePrefab, _hit.point, rotation);
+                    }
+                    else if (_equippedBuilding == Buildings.Cotton)
+                    {
+                        _outlineBuilding = _world.GetComponent<GameManager>().instantiateOnWorld(_world.GetComponent<GameManager>()._cottonPrefab, _hit.point, rotation);
+                    }
+                    else
+                    {
+                        Debug.Log("ERROR: Invalid building type");
+                    }
+                    _outlineBuilding.AddComponent<buildingPlacementValidator>();
+                }
+                //If the outline building has already been instantiated
+                else
+                {
+                    if (_buildingCollisions == 0)
+                    {
+                        foreach (Transform child in _outlineBuilding.transform)
+                        {
+                            child.gameObject.GetComponent<Renderer>().material.color = Color.green;
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log(_buildingCollisions);
+
+                        foreach (Transform child in _outlineBuilding.transform)
+                        {
+                            child.gameObject.GetComponent<Renderer>().material.color = Color.red;
+                        }
+                        _outlineBuilding.transform.Rotate(Vector3.up, 15 * Time.deltaTime);
+                    }
+                    _outlineBuilding.transform.position = _hit.point;
+                    _outlineBuilding.transform.rotation = rotation;
+                    _outlineBuilding.transform.Rotate(90, 0, 0);
+
+                }
+            }
+            #endregion
+
+            #region onTriggerPress
+            //If that object is a button set active Item to that button item
             if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger) == true && triggerHasBeenReleased)
             {
                 triggerHasBeenReleased = false;
 
-                if (collidedObject.name == "FactoryButton")
+                if (collidedObject.name == "World")
+                {
+                    if (_buildingCollisions == 0)
+                    {
+                        instantiateOnWorld();
+                    }
+                    else
+                    {
+                        Debug.Log("Can't build there!");
+                        //Notify player that building placement invalid
+                    }
+                }
+                else if (collidedObject.name == "FactoryButton")
                 {
                     _equippedBuilding = Buildings.Factory;
                 }
@@ -79,22 +166,42 @@ public class BuildingPlacerRay : MonoBehaviour
                 {
                     _equippedBuilding = Buildings.Cotton;
                 }
-                else if (collidedObject.name == "World"){
-                    instantiateOnWorld();
-                }
+
             }
-            else if(OVRInput.Get(OVRInput.RawButton.RIndexTrigger) == false){
+            else if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger) == false)
+            {
                 triggerHasBeenReleased = true;
             }
-        
+            #endregion
+
         }
-        //Ray doesn't collide with anything
+        //Ray doesn't hit anything
         else
         {
+            //Destroy building highlight
+            Destroy(_outlineBuilding);
+            _buildingCollisions = 0;
+            _lineRenderer.SetPosition(1, Vector3.forward * 7);
             _lineRenderer.material.color = Color.red;
         }
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /* 
         void rayCollisionHandler(){
 
@@ -182,7 +289,6 @@ public class BuildingPlacerRay : MonoBehaviour
         }
         else if (_equippedBuilding == Buildings.Factory)
         {
-
             _world.GetComponent<GameManager>().addFactory(_hit.point, rotation);
         }
         else if (_equippedBuilding == Buildings.Farm)
@@ -197,13 +303,14 @@ public class BuildingPlacerRay : MonoBehaviour
         {
             _world.GetComponent<GameManager>().addTree(_hit.point, rotation);
         }
-        else if (_equippedBuilding == Buildings.Tree)
+        else if (_equippedBuilding == Buildings.Cotton)
         {
+            Debug.Log("Cotton placed");
             _world.GetComponent<GameManager>().addCotton(_hit.point, rotation);
         }
         else
         {
-            Debug.Log("ERROR: [BuildingPlacerRay.cs - instantiateOnWorld] invalid building type");
+            Debug.Log("ERROR: Invalid building type");
         }
     }
 
