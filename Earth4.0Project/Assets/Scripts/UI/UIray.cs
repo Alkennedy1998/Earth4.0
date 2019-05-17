dfc7746ca09dfb5632464cdb24f935d24abfb942
Alexander Kennedy <rexer1998@gmail.com>
2019-05-16 23:18:34 -0700

    Start menu now works

--- Earth4.0Project/Assets/Scripts/UI/UIray.cs
+++ Earth4.0Project/Assets/Scripts/UI/UIray.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class UIray : MonoBehaviour {
 
+	public LineRenderer _ray;
+	
+	public GameObject _marker;
+
+	public float maximumLength = 10;
 	// Use this for initialization
 	void Start () {
-		
+		_ray = GetComponent<LineRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		_ray.SetPosition(0, transform.position);
+		_ray.SetPosition(1,_marker.transform.position);
 	}
 }
