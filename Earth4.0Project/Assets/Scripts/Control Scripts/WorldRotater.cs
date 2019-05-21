72322ed53b2f631a9b50776240bfffd241637bf1
Alexander Kennedy <rexer1998@gmail.com>
2019-05-21 04:29:13 -0700

    reduce lag when not rotating world

--- Earth4.0Project/Assets/Scripts/Control Scripts/WorldRotater.cs
+++ Earth4.0Project/Assets/Scripts/Control Scripts/WorldRotater.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class WorldRotater : MonoBehaviour
 {
+    GameObject _AStar;
+    Pathfinding.NavMeshGraph graph;
 
     // Use this for initialization
     void Start()
     {
-
+        _AStar = GameObject.Find("A*");
+        graph = _AStar.GetComponent<AstarPath>().graphs[0] as Pathfinding.NavMeshGraph;
     }
 
     // Update is called once per frame
@@ -22,7 +25,14 @@ public class WorldRotater : MonoBehaviour
         Vector2 leftStick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
         float leftRotate = Time.deltaTime * leftStick.x * -30;
         float rightRotate = Time.deltaTime * leftStick.y * 30;
-        transform.Rotate(0, leftRotate, rightRotate, Space.World);
-    }
 
+        Vector3 before = transform.rotation.eulerAngles;
+        transform.Rotate(0, leftRotate, rightRotate, Space.World);
+        Vector3 after = transform.rotation.eulerAngles;
+
+        if (leftRotate != 0.0f || rightRotate != 0.0f) {
+            graph.rotation += after - before;
+            _AStar.GetComponent<AstarPath>().Scan();
+        }
+    }
 }
